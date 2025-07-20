cc2e7a1655daf45b5d008e2f57321821f4153603
sagar <vyassagar4321@gmail.com>
2025-07-20 23:37:38 +0530

    added publish and subscribe method for rebbitMQ

--- src/index.js
+++ src/index.js
@@ -2,30 +2,24 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const { PORT } = require('./config/serverConfig');
+const { createChannel } = require('./utils/messageQueue');
 const apiRoutes = require('./routes/index');
-// const { sendBasicEmail } = require('./services/email-service');
 const jobs = require('./utils/job');
 
-const startServer = () => {
+const startServer = async () => {
     const app = express();
         
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded( {extended: true} ));
+
+    const channel = await createChannel();
     
     app.use('/api', apiRoutes);
 
     app.listen(PORT, () => {
         console.log(`server running at port ${PORT}`);
         jobs();
-        // sendBasicEmail(
-        //     'support@admin.com',
-        //     'sagavyas321@gmail.com',
-        //     'Testing email',
-        //     'Hey man!'
-        // );
-        // cron.schedule('* * * * *', () => {
-        //     console.log('running a task every minute');
-        // });
+        
     });
 }
 
--- src/utils/messageQueue.js
+++ src/utils/messageQueue.js
@@ -1,5 +1,7 @@
 const amqplib = require('amqplib');
 
+const { MESSAGE_BROKER_URL, EXCHANGE_NAME } = require('../config/serverConfig');
+
 const createChannel = async () => {
     try {
         const connection = await amqplib.connect(MESSAGE_BROKER_URL);
@@ -7,22 +9,37 @@ const createChannel = async () => {
         await channel.assertExchange(EXCHANGE_NAME, 'direct', false);
         return channel;
     } catch (error) {   
-        console.log(error);
+        throw error;
     }
 }
 
 const subscribeMessage = async (channel, service, binding_key) => {
-    const applicationQueue = await channel.assertQueue(QUEUE_NAME);
+    try {
+        const applicationQueue = await channel.assertQueue('QUEUE_NAME');
     
-    channel.bindQueue(applicationQueue.queue, EXCHANGE_NAME, binding_key);
-    channel.consume(applicationQueue.queue, msg => {
-        console.log('received data');
-        console.log(msg.content.toString());
-        channel.ack(msg);
-    });
+        channel.bindQueue(applicationQueue.queue, EXCHANGE_NAME, binding_key);
+        channel.consume(applicationQueue.queue, msg => {
+            console.log('received data');
+            console.log(msg.content.toString());
+            channel.ack(msg);
+        });
+    } catch (error) {
+        throw error;
+    }
+    
+}
+
+const publishMessage = async (channel, binding_key, message) => {
+    try {
+        await channel.assertQueue(QUEUE_NAME);
+        await channel.publish(EXCHANGE_NAME, binding_key, Buffer.from(message));
+    } catch (error) {
+        throw error;
+    }
 }
 
 module.exports = {
     createChannel,
-    subscribeMessage
+    subscribeMessage,
+    publishMessage
 }
