04983b8ca930662e18b22b973dd2baedbe922f16
sagar <vyassagar4321@gmail.com>
2025-07-20 22:06:31 +0530

    added rabbitMQ and setup code for it

---
+++ src/utils/messageQueue.js
@@ -0,0 +1,28 @@
+const amqplib = require('amqplib');
+
+const createChannel = async () => {
+    try {
+        const connection = await amqplib.connect(MESSAGE_BROKER_URL);
+        const channel = await connection.createChannel();
+        await channel.assertExchange(EXCHANGE_NAME, 'direct', false);
+        return channel;
+    } catch (error) {   
+        console.log(error);
+    }
+}
+
+const subscribeMessage = async (channel, service, binding_key) => {
+    const applicationQueue = await channel.assertQueue(QUEUE_NAME);
+    
+    channel.bindQueue(applicationQueue.queue, EXCHANGE_NAME, binding_key);
+    channel.consume(applicationQueue.queue, msg => {
+        console.log('received data');
+        console.log(msg.content.toString());
+        channel.ack(msg);
+    });
+}
+
+module.exports = {
+    createChannel,
+    subscribeMessage
+}
